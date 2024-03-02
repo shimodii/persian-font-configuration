e0ba9d1ace3326f529091d625d19ba7c8e5f30fc
am-shm <am.shahmoradi93@gmail.com>
2024-03-02 23:19:50 +0330

    extacting and copying sections added with unzip checker.

--- configure.sh
+++ configure.sh
@@ -20,8 +20,28 @@ if [[ $(ls /usr/bin | grep -x unzip) == "unzip" ]] ;then
 else
     echo "NO."
     echo "! please install unzip package then run the script !"
+    exit
 fi
 
+# Copying configuration sample
+cp fontsConfigurationSample $HOME/.fonts.conf
 
+# unziping fonts
+mkdir /tmp/pefco
+mkdir $HOME/.fonts > /dev/null
+cp vazirmatn.zip /tmp/pefco
+cd /tmp/pefco
+printf "Extracting the zip file: "
+unzip vazirmatn.zip > /dev/null
 
+if [[ $? == 0 ]] ;then
+    echo "DONE"
+else
+    echo "! Something wrong happend, please check unzip installed correctly on your computer !"
+    exit
+fi
 
+# Copying fonts
+cp ttf/* $HOME/.fonts
+
+echo "DONE. <3"
