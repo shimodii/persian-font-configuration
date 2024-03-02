5547187ada2122aac187b3f309fb08cb28b0e268
am-shm <am.shahmoradi93@gmail.com>
2024-03-02 23:03:31 +0330

    unzip checking part added

--- configure.sh
+++ configure.sh
@@ -10,4 +10,18 @@ echo "|  __/| |___|  _|| |__| |_| |"
 echo "|_|   |_____|_|   \\____\\___/ "
 echo ""
 echo " Persian font configuration"
+echo ""
+echo "Checking dependencies:"
+printf "unzip: "
+
+# Checking unzip installed
+if [[ $(ls /usr/bin | grep -x unzip) == "unzip" ]] ;then
+    echo "OK"
+else
+    echo "NO."
+    echo "! please install unzip package then run the script !"
+fi
+
+
+
 
